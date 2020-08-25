a32c1c52d712e455bad9ce970bb528ff9b90c9e9
Bryce Jensen <brycewjensen@gmail.com>
2020-08-25 16:13:41 -0500

    ran into issues with truffle, issues with giving values await instruction

---
+++ contracts/TodoList.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+  //initialize a new state variable to 0, any data stored in this variable will be written to the blockchain
+  uint taskCount = 0;
+}
